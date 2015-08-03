4f6cef0944bd01ef20732e6c2935a2bd5d4bc5c4
Vytenis Bivainis <monrealis@gmail.com>
2015-08-04 00:34:29 +0300

    Add histogram.

--- octave/pirminiai.m
+++ octave/pirminiai.m
@@ -1,8 +1,9 @@
 numbers = [];
 counts = [];
-for a = 1:200
+for a = 1:10000
   s = size(factor(a), 2);
   counts = [counts s];
   numbers = [numbers a];
 endfor
 plot(numbers, counts);
+hist(counts, min(counts):max(counts));
